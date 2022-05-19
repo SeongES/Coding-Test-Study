00ed4fb4f8a5b5cdc51c60a7eb2241c95a73f2fa
SeongES <81518525+SeongES@users.noreply.github.com>
2022-05-19 11:35:55 +0900

    Update 더 맵게.py

--- Seong/Heap/더 맵게.py
+++ Seong/Heap/더 맵게.py
@@ -18,3 +18,13 @@ def solution(scoville, K):
         heapq.heappush(scoville,mep)
         answer += 1
     return answer
+
+
+'''
+python - heapq 모듈 : binary tree 기반의 min heap 자료구조 제공
+heapq.heappush(heap,7) - heap 에 7 추가
+heapq.heappop(heap) - 가장 작은 원소 삭제
+heapq.heapify(heap) - 리스트를 heap으로 변환
+
+참고 사이트 : https://www.daleseo.com/python-heapq/
+'''
