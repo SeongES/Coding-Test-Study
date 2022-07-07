d0a3d52d22427fd388055c597ec1714f07ad2ea5
Park_byeongtae <65391681+prstoa@users.noreply.github.com>
2022-07-07 12:18:39 +0900

    Update 네트워크.py

--- Park/programmers/practice/네트워크.py
+++ Park/programmers/practice/네트워크.py
@@ -12,5 +12,5 @@ def DFS(n, computers, com, visited):
     visited[com] = True # 연결된 컴퓨터는 True로 6번째 if문에 빠지지 않음.
     for connect in range(n):
         if connect != com and computers[com][connect] == 1: #연결된 컴퓨터
-            if visited[connect] == False:
+            if visited[connect] == False: # 2 > 1로 거꾸로 못 감.
                 DFS(n, computers, connect, visited) # 자식 노드로 내려 감.
