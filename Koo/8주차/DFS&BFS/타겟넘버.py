5e576ae0607a8cc4f5ea046d54addd17cc6cf737
Goo-Goo <56628106+Goo-Goo@users.noreply.github.com>
2022-07-07 11:42:38 +0900

    Create 타겟넘버.py

---
+++ Koo/8주차/DFS&BFS/타겟넘버.py
@@ -0,0 +1,14 @@
+def solution(numbers, target):
+    answer = 0
+    tree = [0]
+    
+    for i in range(len(numbers)) :
+        value = []
+        
+        for j in range(len(tree)) :
+            value.append(tree[j] - numbers[i])
+            value.append(tree[j] + numbers[i])
+        tree = value
+        
+    answer = tree.count(target)
+    return answer
